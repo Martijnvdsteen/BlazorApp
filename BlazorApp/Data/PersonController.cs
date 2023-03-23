8c1d7169b91869efa60958825abc54481e69c341
Martijnvdsteen <martijnvdsteen@florisoft.nl>
2023-03-23 10:33:14 +0100

    Initial commit

---
+++ BlazorApp/Data/PersonController.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorApp.Data
+{
+    public class PersonController
+    {
+        private static readonly string[] Naam = new[]
+        {
+        "Martijn", "Henk", "Joris", "John"
+        };
+        private static readonly string[] Mail = new[]
+        {
+        "MartijnvdSteen@florisoft.nl", "HenkvanDijk@florisoft.nl", "JorisdeJong@florisoft.nl","JohnvanHulst@florisoft.nl"
+        };
+        private static readonly string[] Functie = new[]
+        {
+        "Web developer", "Web developer", "Consultant","Backend developer"
+        };
+
+        public static Task<PersonModel[]> GetPersonsAsync()
+        {
+            return Task.FromResult(Enumerable.Range(0, 4).Select(index => new PersonModel
+            {
+                Organisatie = "Florisoft",
+                Functie = Functie[index],
+                Naam = Naam[index],
+                Email = Mail[index],
+                Foto = "../Person_placeholder_man.jpg",
+                ID = Random.Shared.Next(200, 555)
+
+            }).ToArray());
+        }
+    }
+}
---
+++ BlazorApp/Data/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Data
+{
+    public class PersonModel
+    {
+        public int ID { get; set; }
+
+        public string Naam { get; set; } = "";
+        public string Foto { get; set; }
+        public string Functie { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Organisatie { get; set; } = "";
+    }
+}
